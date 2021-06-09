99f4400afd9d4941224a17f4d7a091fc7bad2127
sunitee <suniteebajpai2@gmail.com>
2021-06-09 19:08:43 -0400

    Done.

--- js/createStore.js
+++ js/createStore.js
@@ -1,4 +1,28 @@
-let state;
+//let state;
+
+//****Redux works by having an action dispatched, 
+//** ...which calls a reducer, and then renders the view. 
+//**...Our createStore's dispatch method does that.
+//CreateStore method_ is a generic method which makes call to dispatch should call a reducer, reassign the state, and render a change.
+
+function createStore(reducer){
+  let state;
+  //state is now accessible to dispatch
+  //closure=> dispatch function has access to variable: state defined with in the parent function scope
+  function dispatch(action){
+    state = reducer(state, action);
+    render();
+  };
+  function getState() {
+    return state;
+  }
+
+  return { 
+    dispatch,
+    getState 
+  }
+
+}
 
 function reducer(state = { count: 0 }, action) {
   switch (action.type) {
@@ -10,19 +34,16 @@ function reducer(state = { count: 0 }, action) {
   }
 };
 
-function dispatch(action){
-  state = reducer(state, action);
-  render();
-};
 
 function render() {
   let container = document.getElementById('container');
-  container.textContent = state.count;
+  container.textContent = store.getState().count;
 };
 
-dispatch({ type: '@@INIT' })
-let button = document.getElementById('button');
+let store = createStore() // createStore takes the reducer as an argument
+  store.dispatch({ type: '@@INIT' })
 
+let button = document.getElementById('button');
 button.addEventListener('click', function() {
-    dispatch({ type: 'INCREASE_COUNT' });
+    store.dispatch({ type: 'INCREASE_COUNT' });
 })
